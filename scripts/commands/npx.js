3ca9b1bea04ce45254e573619d0f0d304b6e7e30
Niyaz897 <162999391+Niyaz897@users.noreply.github.com>
2024-10-17 14:12:29 +0600

    Update npx.js

--- scripts/commands/npx.js
+++ scripts/commands/npx.js
@@ -16,7 +16,7 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-	if (body.indexOf("Niyaz Id")==0 || body.indexOf("Boss id")==0 || body.indexOf("niyaz id")==0 || body.indexOf("")==0) {
+	if (body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf("😒")==0) {
 		var msg = {
 				body: "Niyaz Boss ar Id 👇",
 				attachment: fs.createReadStream(__dirname + `/Nayan/Niyazp.png`)
